481e1410f22cb7ee1f7b8cc65761f5a74423a278
rms13 <rishabhs@seas.upenn.edu>
2017-05-12 23:20:03 -0400

    Particle visualizer
    - music (.mp3) using web audio api
    - mapping frequencies to points, and blending with sine wave

---
+++ src/framework.js
@@ -0,0 +1,100 @@
+const THREE = require('three');
+const OrbitControls = require('three-orbit-controls')(THREE)
+import Stats from 'stats-js'
+import DAT from 'dat-gui'
+
+//////////////
+// Sound:
+var analyser;
+var data;
+var aud;
+window.onload = function() {
+  var audcon = new AudioContext();
+  aud = document.getElementById('myAudio');
+  var audsrc = audcon.createMediaElementSource(aud);
+  analyser = audcon.createAnalyser();
+
+  audsrc.connect(analyser);
+  audsrc.connect(audcon.destination);
+  data = new Uint8Array(analyser.frequencyBinCount); // read audio data.. 1024B by default..
+
+  //aud.play();
+};
+//////////////
+
+// when the scene is done initializing, the function passed as `callback` will be executed
+// then, every frame, the function passed as `update` will be executed
+function init(callback, update) {
+  var stats = new Stats();
+  stats.setMode(1);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+
+  var gui = new DAT.GUI();
+
+  var framework = {
+    gui: gui,
+    stats: stats
+  };
+
+  // run this function after the window loads
+  window.addEventListener('load', function() {
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xffffff,1 );
+
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enableZoom = true;
+    controls.target.set(0, 0, 0);
+    controls.rotateSpeed = 0.3;
+    controls.zoomSpeed = 1.0;
+    controls.panSpeed = 2.0;
+
+    document.body.appendChild(renderer.domElement);
+
+    // resize the canvas when the window changes
+    window.addEventListener('resize', function() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    // assign THREE.js objects to the object we will return
+    framework.scene = scene;
+    framework.camera = camera;
+    framework.renderer = renderer;
+	framework.aud=aud;
+
+
+    // begin the animation loop
+    (function tick() {
+      stats.begin();
+
+	  analyser.getByteFrequencyData(data); // read audio data.. 1024B by default..
+	  framework.data=data;
+	  //console.log(data);
+
+	  update(framework); // perform any requested updates
+      renderer.render(scene, camera); // render the scene
+      stats.end();
+      requestAnimationFrame(tick); // register to call this again when the browser renders a new frame
+    })();
+
+    // we will pass the scene, gui, renderer, camera, etc... to the callback function
+    return callback(framework);
+  });
+}
+
+export default {
+  init: init
+}
+
+export const PI = 3.14159265
+export const e = 2.7181718
---
+++ src/main.js
@@ -0,0 +1,153 @@
+const THREE = require('three'); // older modules are imported like this. You shouldn't have to worry about this much
+import Framework from './framework'
+
+// r,g,b to pass to shaders
+var r=0.6;
+var g=0.0;
+var b=0.0;
+
+// function to manipulate stuff from gui
+var GUIoptions = function()
+{
+	this.Red=0.6;
+	this.Green=0.0;
+	this.Blue=0.0;
+	this.Music=false;
+	this.MusicSource=function(){
+		window.location = "http://freemusicarchive.org/music/The_Kyoto_Connection/Wake_Up_1957/09_Hachiko_The_Faithtful_Dog";};
+}
+
+// for time calculations
+var oldt=0.0;
+var newt=0.0;
+var time=0.0;
+
+// material for geometry
+// var icoshMaterial = new THREE.ShaderMaterial({
+//     uniforms: {
+//     //   image: { // Check the Three.JS documentation for the different allowed types and values
+//     //     type: "t",
+//     //     value: THREE.ImageUtils.loadTexture('./adam.jpg')
+//     //   },
+// 	  time: {value : 0.0},
+// 	  Red: {value : 0.6},
+// 	  Green: {value : 0.0},
+// 	  Blue: {value : 0.0},
+// 	  data: {
+// 		  type : 'iv1',
+// 		  value : new Array}
+//     },
+//     vertexShader: require('./shaders/line-visualizer-vert.glsl'),
+//     fragmentShader: require('./shaders/line-visualizer-frag.glsl')
+//   });
+
+var vertList = [];
+var material = new THREE.PointCloudMaterial({	size: 0.25, vertexColors: THREE.VertexColors });
+var geometry = new THREE.Geometry();
+var pointCloud = new THREE.PointCloud(geometry, material);
+
+// called after the scene loads
+function onLoad(framework) {
+  var scene = framework.scene;
+  var camera = framework.camera;
+  var renderer = framework.renderer;
+  var gui = framework.gui;
+  var stats = framework.stats;
+  var data= framework.data; // per frame audio data
+  var aud= framework.aud; // audio object to control play/pause
+
+  var {scene, camera, renderer, gui, stats, data, aud} = framework;
+
+  // initialize an icosahedron and material
+  //var icosh = new THREE.IcosahedronBufferGeometry(1, 5);
+  //var icosh = new THREE.Mesh(icosh, icoshMaterial);
+
+  //var material = new THREE.PointCloudMaterial({	color: 0xffffcc });
+
+
+	var x, y, z;
+	for(var i=0;i<10240*2;i++)
+	{
+	  x = (Math.random() * 1000) - 500;
+	  y = 0;//(Math.random() * 800) - 400;
+	  z = (Math.random() * 1000) - 500;
+	  var vert = new THREE.Vector3(x, y, z);
+	  vertList.push(vert);
+	  geometry.vertices.push(vert);
+	  geometry.colors.push(new THREE.Color(0,0,0));//(Math.random(), Math.random(), Math.random()));
+	}
+
+
+	scene.add(pointCloud);
+
+  // set camera position
+  camera.position.set(0,10,50);
+  camera.lookAt(new THREE.Vector3(0,0,0));
+
+  // add icosh to the scene
+  //scene.add(icosh);
+
+  // Elements for the GUI:
+  gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
+    camera.updateProjectionMatrix();
+  });
+  var update= new GUIoptions();
+  gui.add(update,'Red', 0.0, 1.0,0.05).onChange(function(newVal) {
+    r=newVal;
+  });
+  gui.add(update,'Green', 0.0, 1.0,0.05).onChange(function(newVal) {
+    g=newVal;
+  });
+  gui.add(update,'Blue', 0.0, 1.0,0.05).onChange(function(newVal) {
+    b=newVal;
+  });
+  gui.add(update,'Music').onChange(function(newVal) {
+    if(newVal===false) aud.pause();
+	else aud.play();
+  });
+  gui.add(update,'MusicSource').onclick;
+}
+
+// called on frame updates
+function onUpdate(framework) {
+   // icoshMaterial.uniforms.Red.value=r;
+   // icoshMaterial.uniforms.Green.value=g;
+   // icoshMaterial.uniforms.Blue.value=b;
+
+   oldt=newt;
+   newt=performance.now(); // measures time since the beginning of execution
+   time+=(newt-oldt);
+
+   // icoshMaterial.uniforms.data.value=Int32Array.from(framework.data); // typed arrays casting
+  // icoshMaterial.uniforms.time.value=time/4000; // control the speed of cloud movement
+
+   /// NEW
+   for(var i=0; i<geometry.vertices.length;i++)
+   {
+    var dX, dY, dZ;
+    //dX = Math.random() - 0.5;
+    //dY = Math.random() - 0.5;
+    //dZ = Math.random() - 0.5;
+	var s = 2*Math.sin(vertList[i].z/20+time/2000);
+	dX = vertList[i].x + 0;
+	dZ = vertList[i].z + 0;
+	dY = vertList[i].y
+		+ 10 * s * (framework.data[i%100]/2+framework.data[(i-1)%100]/4+framework.data[(i+1)%100]/4)/255.0
+		+ 2 * s;
+	//	+ s * Math.random()/2 - 0.25;
+
+	//var col = Math.abs(vertList[i].x*vertList[i].z);
+	if(dY>0)
+		geometry.colors[i] = new THREE.Color(vertList[i].y,vertList[i].y,vertList[i].y);//(Math.random(), Math.random(), Math.random());
+	//else
+		//geometry.colors[i] = new THREE.Color(1,1,1);
+
+	//var vert = new THREE.Vector3(dX,dY,dZ);
+    geometry.vertices[i] = new THREE.Vector3(dX,dY,dZ);
+   };
+   geometry.verticesNeedUpdate = true;
+   geometry.colorsNeedUpdate = true;
+}
+
+// when the scene is done initializing, it will call onLoad, then on frame updates, call onUpdate
+Framework.init(onLoad, onUpdate);
